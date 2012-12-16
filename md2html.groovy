ba14749dfaf624c7779f5730ecc70301b6977ab7
nemo10 <nemo10+R500@gmail.com>
2012-12-16 20:28:48 +0900

    Initial commit

---
+++ md2html.groovy
@@ -0,0 +1,30 @@
+
+// Markdown to HTML converter
+// Usage example : md2html.groovy README.md
+// README.md.html will be generated
+
+
+@Grapes([
+//    @Grab('org.markdownj:markdownj:0.3.0-1.0.2b4')
+      @Grab('org.markdownj:markdownj:latest.release')
+])
+import com.petebevin.markdown.*
+
+if (args.length ==0) {
+    println """Markdown to HTML converter
+Usage: md2html.groovy <Makrdown filename>"""
+    return   
+} 
+m = new MarkdownProcessor(); 
+outputMd =""
+inputMd = new File(args[0]).eachLine { line  -> 
+   outputMd += line +"\n"
+}
+String html = m.markdown(outputMd)
+
+println html
+
+htmlFile=new File(args[0]+".html").newWriter()
+htmlFile.writeLine(html)
+htmlFile.close()
+
